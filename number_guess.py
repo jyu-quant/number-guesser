b4c67cb569e4c0f2e8d48746817729ec3369b333
jyu-quant <61554802+jyu-quant@users.noreply.github.com>
2020-02-27 14:19:49 -0500

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,23 @@
+""" number guessing game from 1-100 where number is randomly generated """
+import numpy as np
+np.random.seed()
+
+def main(): #initializes program
+    print('This is a number guessing game')
+    print('A random integer from 1 to 100 has been generated')
+    print('Guess the number')
+    rand_num = np.random.randint(1,101) #generates a random integer
+    found = False                       #from 1 to 100
+    while not found:
+        guess = eval(input('Your guess: '))
+        if guess == rand_num:
+            print('You got it!')
+            found = True
+        elif guess < rand_num:
+            print('Guess higher.')
+        else:
+            print('Guess lower.')
+        
+    
+if __name__ == '__main__':
+    main()
